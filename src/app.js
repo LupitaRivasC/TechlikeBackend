49d1bd0d580ce20105a0bdb2e3cf6ba3dcf2da6c
LupitaRivasC <l20450660@itz.edu.mx>
2024-12-18 04:36:04 -0600

    Ajustes Navbar

--- src/app.js
+++ src/app.js
@@ -12,13 +12,13 @@ import authRoutes from './routes/auth.routes.js';
 import productRoutes from './routes/products.routes.js';
 import categoryRoutes from './routes/category.routes.js';
 import cartRoutes from './routes/cart.routes.js';
-import perfilRoutes from './routes/perfil.routes.js';
 
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:4000'],
-  credentials: true,
+  origin: ['http://localhost:5173', 'http://localhost:4000','http://localhost',
+   process.env.BASE_URL],
+ credentials: true
 }));
 
 app.use(morgan('dev'));
@@ -32,5 +32,4 @@ app.use('/api/', authRoutes);
 app.use('/api/', productRoutes);
 app.use('/api/', categoryRoutes);
 app.use('/api/', cartRoutes);
-app.use('/api/',perfilRoutes)
 export default app;
--- src/db.js
+++ src/db.js
@@ -2,9 +2,9 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
     try {
-        //const url = 'mongodb+srv://Sistema_Productos:SistemaProductos.2024@cluster0.iu95u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-        await mongoose.connect('mongodb://127.0.0.1/techlike')
-        //await mongoose.connect(url);
+        const url = 'mongodb+srv://techlike:techlike12345@cluster0.wkwol.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+        //const url = 'mongodb://127.0.0.1/techlike';
+        await mongoose.connect(url);
         console.log("Base de datos conectada correctamente");
     } catch (error) {
         console.log(error);
