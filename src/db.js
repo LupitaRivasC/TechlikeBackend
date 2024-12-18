c99c92b297f3e08d1aa08277b1d8859aa7fe5286
LupitaRivasC <l20450660@itz.edu.mx>
2024-12-18 06:00:41 -0600

    1

--- src/db.js
+++ src/db.js
@@ -1,9 +1,8 @@
 import mongoose from 'mongoose';
 
 export const connectDB = async () => {
-    try {
-        //const url = 'mongodb+srv://techlike:techlike12345@cluster0.wkwol.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-        //const url = 'mongodb://127.0.0.1/techlike';
+        //const url ='mongodb://127.0.0.1/sistemainfo'
+        //const url = 'mongodb+srv://Sistema_Productos:Sistema@cluster0.iu95u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
         const url = process.env.MONGODB_URL;
         await mongoose.connect(url)
                       .then(()=>{
@@ -15,4 +14,3 @@ export const connectDB = async () => {
                 }
         //await mongoose.connect(url);
         //console.log("Base de datos conectada correctamente");
-};
