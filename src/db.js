c48fc41a9d3cd9c7d18c03293007ce0bde917cde
LupitaRivasC <l20450660@itz.edu.mx>
2024-12-18 19:56:01 -0600

    Hola

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
     
-        const url = process.env.MONGODB_URL;
+        const url = process.env.MONGO_BD_URL;
         await mongoose.connect(url)
                       .then(()=>{
                         console.log("Base de datos conectada");
